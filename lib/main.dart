f619f4ac1837067547fa7efd72f1f33f1869a034
P024024 <pghimire024@rku.ac.in>
2023-10-05 15:48:50 +0530

    Fixed the app working only on web but not on android added IOS firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,14 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:medical/FirebaseConfig/firebase_options.dart';
 import 'package:medical/login/signin_page.dart';
 import 'package:medical/nav_home_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
+  await Firebase.initializeApp();
   runApp(MyApp());
+
 }
 
 class MyApp extends StatefulWidget {
